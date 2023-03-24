e97dd3fa2bb2f02307d5e8defb16d5236ff11c7e
MaxyGrosman <maxygrosman@gmail.com>
2023-03-24 12:28:18 -0300

    Rearrange folders

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import SearchBar from "../SearchBar/SearchBar";
+import SearchBar from "./SearchBar/SearchBar";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Nav({onSearch, logout}){
