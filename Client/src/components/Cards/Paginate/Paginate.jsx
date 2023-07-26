5dccf290bc3ec6bc793f1ac9ecf3fe918c15f447
MaxyGrosman <maxygrosman@gmail.com>
2023-07-26 18:46:56 -0300

    src/Client 26/7 18:46

--- Client/src/components/Cards/Paginate/Paginate.jsx
+++ Client/src/components/Cards/Paginate/Paginate.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {nextPage, prevPage} from "../../../redux/actions/actions";
+import {getPageCharacter} from "../../../redux/actions/actions";
 import style from './Paginate.module.css';
 
-export default function Paginate({cantPages}){
+export default function Paginate(){
 
-    const {numPage} = useSelector((state) => state)
+    const {numPage, cantPage} = useSelector((state) => state)
     const dispatch = useDispatch();
 
-    function next(){
-        dispatch(nextPage());
-    };
-    function prev(){
-        dispatch(prevPage());
-    };
+    function next(){dispatch(getPageCharacter(numPage + 1))};
+    function prev(){dispatch(getPageCharacter(numPage - 1))};
+    function first(){dispatch(getPageCharacter(1))};
+    function last(){dispatch(getPageCharacter(cantPage))};
 
     return(
         <div className={style.contains}>
 
-            { numPage > 1 ? (
-                <button className={style.button}  onClick={prev}>{numPage - 1}</button>
-            ) : null  }
+            {numPage != 1 ? (<button className={style.button} onClick={first}>First
+            </button>) : null}
+
+            {numPage > 1 ? (<button className={style.button} onClick={prev}>{"<-"}</button>
+                ) : null}
             
             <h3 className={style.button}>{numPage}</h3>
             
-            { numPage < cantPages ? ( 
-                <button className={style.button}  onClick={next}>{numPage + 1}</button>
-            ) : null }
+            {numPage < cantPage ? (<button className={style.button} onClick={next}>{"->"}
+                </button>) : null}
+
+            {numPage < cantPage ? (<button className={style.button} onClick={last}>Last
+                </button>) : null }
         </div>
     );
 };
