bf7a72115256ee4d52ef5485274818a3e2268397
MaxyGrosman <maxygrosman@gmail.com>
2023-08-02 19:52:40 -0300

    Server/src/handlers/handlerCharacter.js 2/8 19:52

--- Server/src/handlers/handlerCharacter.js
+++ Server/src/handlers/handlerCharacter.js
@@ -18,6 +18,7 @@ const allCharacters = async() => {
             charactersCache = [...charactersCache, ...load];
         };
         cantPage = Math.ceil(charactersCache.length / 6);
+        showCharacters = charactersCache;
         console.log('cache loaded', cantPage);
         return charactersCache;
     }catch(error){console.log(error)};
@@ -43,7 +44,7 @@ const getCharactersPage = (req, res) => {
         if(page <= 0) return res.status(404).json({message: "This page doesn't exist"});
         if(page > cantPage) return res.status(404).json({message: "This page doesn't exist"});
         let from = page - 1;
-        let response = {characters: charactersCache.slice(from * 6, page * 6), cantPage};
+        let response = {characters: showCharacters.slice(from * 6, page * 6), cantPage};
         res.status(200).json(response);
     }catch(error){ res.status(500).json(error)};
 };
@@ -51,20 +52,26 @@ const getCharactersPage = (req, res) => {
 const getCharacterName = async(req, res) => {
     try{
         const {name} = req.query;
-        let {data} = await findCharacterByName(name);
-        let {info} = data;
-        let urlNext = info.next;
-        let load = loadRoundOfChar(data.results);
-        charactersCache = [...load];
-        while(urlNext){
-            let {data} = await axios(`${urlNext}`);
+
+        if(name === '') {
+            showCharacters = charactersCache;
+        }else{
+            let {data} = await findCharacterByName(name);
             let {info} = data;
-            urlNext = info.next;
+            let urlNext = info.next;
             let load = loadRoundOfChar(data.results);
-            charactersCache = [...charactersCache, ...load];
+            showCharacters = [...load];
+            while(urlNext){
+                let {data} = await axios(`${urlNext}`);
+                let {info} = data;
+                urlNext = info.next;
+                let load = loadRoundOfChar(data.results);
+                showCharacters = [...showCharacters, ...load];
+            };
         };
-        cantPage = Math.ceil(charactersCache.length / 6);
-        let response = {characters: charactersCache.slice(0, 6), cantPage};
+
+        cantPage = Math.ceil(showCharacters.length / 6);
+        let response = {characters: showCharacters.slice(0, 6), cantPage};
         return res.status(200).json(response);
     }catch(error){res.status(500).json(error)};
 };
@@ -73,25 +80,9 @@ const getCharacterGender = async(req, res) => {
     try{
         const {gender} = req.query;
         if(!gender) return res.status(404).json({error: "Please send a gender"});
-        if(charactersCache.filter((character) => character.gender === gender).length === 0){
-            let {data} = await findCharacterByName(" ");
-            let {info} = data;
-            let urlNext = info.next;
-            console.log(2);
-            let load = loadRoundOfChar(data.results);
-            charactersCache = [...load];
-            while(urlNext){
-                let {data} = await axios(`${urlNext}`);
-                let {info} = data;
-                urlNext = info.next;
-                let load = loadRoundOfChar(data.results);
-                charactersCache = [...charactersCache, ...load];
-            };
-            cantPage = Math.ceil(charactersCache.length / 6);
-        };
-        charactersCache = charactersCache.filter((character) => character.gender === gender);
-        cantPage = Math.ceil(charactersCache.length / 6);
-        let response = {characters: charactersCache.slice(0, 6), cantPage};
+        showCharacters = charactersCache.filter((character) => character.gender === gender);
+        cantPage = Math.ceil(showCharacters.length / 6);
+        let response = {characters: showCharacters.slice(0, 6), cantPage};
         return res.status(200).json(response);
     }catch(error){res.status(500).json(error)};
 };
@@ -99,11 +90,11 @@ const getCharacterGender = async(req, res) => {
 const getSortedCharacters = async(req, res) => {
     try{
         const {sort} = req.query;
-        !sort ? charactersCache.sort((a,b) => a.id - b.id) : sort === "A" ? 
-        charactersCache.sort((a,b) => a.name.localeCompare(b.name)) : 
-        charactersCache.sort((a,b) => b.name.localeCompare(a.name));
+        sort === "" ? showCharacters.sort((a,b) => a.id - b.id) : sort === "A" ? 
+        showCharacters.sort((a,b) => a.name.localeCompare(b.name)) : 
+        showCharacters.sort((a,b) => b.name.localeCompare(a.name));
 
-        let response = {characters: charactersCache.slice(0, 6)};
+        let response = {characters: showCharacters.slice(0, 6)};
         return res.status(200).json(response);
     }catch(error){return res.status(500).json(error)};
 };
